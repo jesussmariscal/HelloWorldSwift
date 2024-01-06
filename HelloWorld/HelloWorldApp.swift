566228b6d096a6b7e86d33303f217640d50826a4
jesussmariscal <mariscalalonso16@icloud.com>
2024-01-06 19:00:32 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jesus Mariscal on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
